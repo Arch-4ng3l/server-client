d86dea7467e3a8d578cc8a18343599aa9cb62ee1
m0er1tz <moritzroettel06@gmail.com>
2023-06-13 08:09:54 +0200

    second commit

--- server/server.c
+++ server/server.c
@@ -13,46 +13,41 @@ int main() {
 	struct sockaddr_in server_addr; 
 	int server_sock, client_sock; 
 	
-	char msg[500] = "HELLO \n";
+	char msg[500]; 
 	char buf[500]; 
 	server_addr.sin_family = AF_INET; 
-	server_addr.sin_port = htons(8080); 
+	server_addr.sin_port = htons(10000); 
 	server_addr.sin_addr.s_addr = inet_addr("192.168.178.20"); 
 	
 	server_sock = socket(AF_INET, SOCK_STREAM, 0);
 	
 	int err = bind(server_sock, (struct sockaddr*) &server_addr, sizeof(server_addr));
-	
+		
 	if (err == -1) {
 		exit(EXIT_FAILURE);
 	}
-	
+	printf("listening \n");
 	listen(server_sock, 10); 
 	
 
 	client_sock = accept(server_sock, NULL, NULL); 
 	
 	printf("accepted \n"); 
-	send(client_sock, msg, sizeof(msg), 0);
 	recv(client_sock, buf, sizeof(buf), 0);
 	printf("recv : %s", buf);
 	
 	while (1) {
 		memset(msg, 0, sizeof(msg)); 
-		printf("[+] SHELL >");
+		printf("[+] SHELL > ");
 		fgets(msg, sizeof(msg), stdin); 
+		
 		send(client_sock, msg, sizeof(msg), 0); 
 
+		recv(client_sock,  buf, sizeof(buf), 0);
+		printf("%s\n", buf);
 		if(strcmp(msg, "quit") >= 0) {
 			printf("closing connection \n"); 
 			break;
 		}
-
-		//memset(buf, 0, sizeof(buf));
-		
-		//recv(client_sock, buf, sizeof(buf), 0);
-
-		//printf("recv : %s", buf);
-
-		}
+	}
 }
